356c7193883b5dd47ddd9ec7c17ec704ecf104a8
clxsh <clxsh19@gmail.com>
2021-01-05 22:54:51 +0530

    reformated abit

--- ball_game.py
+++ ball_game.py
@@ -8,9 +8,6 @@ class Pad:
         self.direction = None
         self.pad_x = WIDTH//2
         self.pad_y = HEIGHT - self.pad_height
-
-    def clear_screen(self,screen):
-        screen.fill((0,0,0))
     
     def draw(self, screen):
         pygame.draw.rect(screen, (255,255,255), pygame.Rect(self.pad_x,self.pad_y, self.pad_width,self.pad_height))
@@ -21,19 +18,22 @@ class Pad:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # checking if key is up
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     self.direction = 'left'
                 if event.key == pygame.K_RIGHT:
                     self.direction = 'right'
 
+            # checking if key is down
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
                     self.direction = None
                 if event.key == pygame.K_RIGHT:
                     self.direction = None
 
-    def turn(self):
+    def move_direction(self):
         if self.direction == 'left':
             self.pad_x -= 10
         elif self.direction == 'right':
@@ -42,6 +42,7 @@ class Pad:
 class Ball:
     def __init__(self):
         self.ball_radius = 8
+        # x and y velocity
         self.velocity = [4,4]
         self.ball_x = WIDTH//2
         self.ball_y = HEIGHT//2
@@ -50,16 +51,18 @@ class Ball:
         self.ball_x += self.velocity[0]
         self.ball_y += self.velocity[1]
 
+        # reverse its velocityif ball_x collides with left or right side 
         if self.ball_x + self.ball_radius > WIDTH or self.ball_x < 0:
             self.velocity[0] = -self.velocity[0]
 
+        # reverse its velocityif ball_y collides with up or down side 
         if self.ball_y < 0:
             self.velocity[1] = -self.velocity[1]
 
+        # reset if you miss the ball
         if self.ball_y + self.ball_radius > HEIGHT:
             self.ball_x = WIDTH//2
             self.ball_y = HEIGHT//2
-
 
     def clear_screen(self,screen):
         screen.fill((0,0,0))
@@ -71,13 +74,19 @@ class Ball:
         self.clear_screen(screen)
 
     def check_collision(self, pad_x, pad_y):
-        ball_bottom = bx1, by1 = self.ball_x, self.ball_y
-        pad_left_vertex = px1, py1 = pad_x, pad_y
-        pad_right_vertex = px2, py2 = (pad_x + 50), pad_y
+        #ball_pos
+        bx1, by1 = self.ball_x, self.ball_y
+        #pad_left_vertex pos
+        px1, py1 = pad_x, pad_y
+        #pad_right_vertex pos
+        px2, py2 = (pad_x + 50), pad_y
 
+        #ac = distance between left-vertex-pos and ball-pos
         ac = math.sqrt( ((bx1-px1)**2) + ((by1-py1)**2) )
+        #cb = distance between right-vertex-pos and ball-pos
         cb = math.sqrt( ((px2-bx1)**2) + ((py2-by1)**2) )
 
+        # check if equal pad_width
         if round(ac+cb) <= 50:
             self.velocity[1] = -self.velocity[1]
 
@@ -92,11 +101,12 @@ def main():
     pad = Pad()
     ball = Ball()
 
+    # Game loop
     while True:
         pad.draw(screen)
         ball.draw(screen)
         ball.check_collision(pad.pad_x, pad.pad_y)
-        pad.turn()
+        pad.move_direction()
         pad.handle_events()
         clock.tick(50)
 
